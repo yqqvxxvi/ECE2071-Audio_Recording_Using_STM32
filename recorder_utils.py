d3a2406015ef9cfaa873deee56079bf7a1e91e42
naamjassv <naam0002@student.monash.edu>
2025-05-14 08:24:32 +0800

    Updated distance_mode function for recorder_utils.py

--- recorder_utils.py
+++ recorder_utils.py
@@ -19,12 +19,13 @@ Written on: 09/05/2025
 Description: Added function headers to explain functions
             Edited playback_menu function to continously show menu after playing song until user quit
             Edited show_menu function to display error message for wrong input
-            TODO: make distance for distance_mode a custom input from user 
-            (send byte of distance to be used and tell processing stm to stop sending data via loop when user outside range)
+            Added: make distance for distance_mode a custom input from user (as per task 2 requirements)
+            Added value check for user input to limit within range 10 - 20
 
 File Header:
             Main file and CLI that user interacts with. Functions are called within this file and outputs (graphs + audios etc) are outputted from this file. 
 '''
+
 #import modules to be used
 import serial
 import matplotlib.pyplot as plt
@@ -101,7 +102,6 @@ None
 
 Note:
 
-
 Example usage:
     receiving_data(ser, "audio_01.bin", "audio_01.csv", "audio_01.png")
 '''
@@ -121,15 +121,13 @@ def receiving_data(ser, binary_filename, csv_filename, plot_filename , sample_ra
         writer.writerow(["SampleRate(Hz)", sample_rate])
         writer.writerow(["Time (s)", "Amplitude (V)"]) #header
         
-        #process serial data
-        while True: 
+        while True:
             try:
                 chunk = ser.read(CHUNK_SIZE)
                 if chunk:
                     bin_file.write(chunk)
                     bytes_read += len(chunk)
                     print(f"Read {len(chunk)} bytes, total: {bytes_read}\n")
-
                     # Extract 12-bit ADC samples (each sample = 2 bytes, little endian)
                     for i in range(0, len(chunk) - 1, 2):
                         adc_val = chunk[i] | (chunk[i + 1] << 8) # decode binary data to 12-bit ADC value manually
@@ -150,7 +148,7 @@ def receiving_data(ser, binary_filename, csv_filename, plot_filename , sample_ra
         print(f"[INFO] Data saved as {binary_filename} in 'output_raw_data' folder\n")
 
 '''
-Converts adc values into voltage values that can be read by STM and to be used for amplitude plotting 
+Function converts adc values into voltage values that can be read by STM and to be used for amplitude plotting 
 
 Parameters: 
 Adc integer values between 0 and 4095 
@@ -168,7 +166,7 @@ def adc_to_amplitude(adc_val):
     return (adc_val / 4095.0) * 3.3  # 12-bit ADC
 
 '''
-Plots data obtained into a graph of Amplitude against Time and saves it as a png file
+Function plots data obtained into a graph of Amplitude against Time and saves it as a png file
 
 Parameters: 
 Amplitude_list = list of float values, amplitude_time = float time values in seconds, filename = file to to write data to
@@ -183,12 +181,12 @@ Example usage:
     plot_data(amplitudes, time_lsit, 'amplitude_time.png')
 '''
 def plot_data(amplitude_list, amplitude_time, filename):
-    plt.figure(figsize=(10, 5)) # sets width and height of figure
+    plt.figure(figsize=(10, 5))
     plt.plot(amplitude_time, amplitude_list, label='Amplitude', color='blue')
     plt.title('ADC Amplitude over Time')
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude (V)')
-    plt.grid() 
+    plt.grid()
     plt.legend()
     plt.savefig(filename)
     print(f"[INFO] Plot saved as {filename}\n")
@@ -210,15 +208,14 @@ Example usage:
         convert_to_wav("recording_20250509_120000.data", recorded_audio_20250509_120000.wav")
 '''
 def convert_to_wav(input_file, output_file):
-
-    #Check if file exists. exist_ok does not raise error if alrdy exists. 
+    # Check if the file exists
     os.makedirs("output_audio", exist_ok=True)
     os.makedirs("output_raw_data", exist_ok=True)
     if not os.path.exists(input_file):
         print(f"[ERROR] Input file '{input_file}' does not exist.")
         return
     
-    # teammates might use cross-platform tools, so need to check the OS. nt for Windows. 
+    # teammates might use cross-platform tools, so need to check the OS
     exe = "./convert_to_wav.exe" if os.name != "nt" else "convert_to_wav.exe"
 
     print(f"[INFO] Running: {exe} {input_file} {output_file}")
@@ -226,14 +223,13 @@ def convert_to_wav(input_file, output_file):
     print(f"[RESULT] Exit code: {result[0]}\n")
     print(f"[RESULT] Output:\n{result[1]}\n")
 
-    #check tuple from result above. 
     if result[0] == 0:
         print(f"[SUCCESS] WAV file saved to: {output_file}\n")
     else:
         print("[ERROR] WAV conversion failed\n")
 
 '''
-Custom user-set duration to record audio in seconds. Processes incoming audio and saves data into raw & wav files.
+Function enables manual mode (custom user-set duration) to record audio in seconds. Processes incoming audio and saves data into raw & wav files.
 
 Parameters: 
 ser = serial port connected to device, user inputs time in int type
@@ -251,15 +247,14 @@ Example usage:
 '''
 def manual_mode(ser):
     duration = input("\nEnter recording duration in seconds (e.g., 5): \n").strip()
-    if not duration.isdigit(): #check for only digits, otherwise return error
+    if not duration.isdigit():
         print("[ERROR] Invalid input. Please enter a number.\n")
         return
     duration_sec = int(duration)
-    cmd = f"manual_{duration_sec:02d}"  #format as 2 digit integer
-    send_command(ser, cmd)  #send to STM
+    cmd = f"manual_{duration_sec:02d}"
+    send_command(ser, cmd)
 
-    #call other functions
-    raw_file, wav_file, _, _ = get_new_filename()  #func returns 4 filenames but only need 2
+    raw_file, wav_file = get_new_filename()
     receiving_data(ser, raw_file)
     convert_to_wav(raw_file, wav_file)
 
@@ -268,12 +263,14 @@ Function sends command of when to record and which mode (distance mode). Display
 
 Parameters: 
 ser = serial port connected to device
+trigger_dis = user set distance for the maximum distance to trigger recording
 
 Returns: 
 None. 
 
 Note: 
 Asumes ser is alrdy set to ser = serial.Serial("COM_", XXXX) for the respective port and baudrate used
+Assumes user sets valid range
 
 Example usage:
     distance_mode(ser)
@@ -281,12 +278,23 @@ Example usage:
     [INFO] Or press Ctrl + C to stop recording.
 '''
 def distance_mode(ser):
+    while True:
+        try:
+            trigger_dis = int(input("[INFO] Set Maximum Distance to trigger recording (Between 10 & 20 cm):\n ").strip())
+            if 10 <= trigger_dis <= 20:
+                break
+            else:
+                print("[ERROR] Please enter a value between 10 and 20.")
+        except ValueError:
+            print("[ERROR] Invalid input. Please enter an integer.")
+
+    send_command(ser, str(trigger_dis)) 
+    print(f"[INFO] Distance trigger mode activated. Will stop if the distance is further than {trigger_dis}cm for 5 seconds.\n")
     send_command(ser, "resume   ")
-    print("[INFO] Distance trigger mode activated. Will stop if the distance is further than 10cm for 5 seconds.\n")
+    print(f"[INFO] Distance trigger mode activated. Will stop if the distance is further than {trigger_dis}cm for 5 seconds.\n")
     print("[INFO] Or press Ctrl + C to stop recording.\n")
 
-    #function calls to save data
-    raw_file, wav_file, _, _ = get_new_filename()
+    raw_file, wav_file = get_new_filename()
     receiving_data(ser, raw_file)
     convert_to_wav(raw_file, wav_file)
 
@@ -307,7 +315,7 @@ Example usage:
 '''
 def interrupt(ser):
     send_command(ser, "interrupt")
-
+    
 '''
 Function runs the specified audio file
 
@@ -325,16 +333,15 @@ Example usage:
 '''
 def play_audio_file(filepath):
     system = platform.system()
-    #for teammates using different OS
     try:
         if system == "Windows":
-            os.startfile(filepath)  #use default player
+            os.startfile(filepath)
         elif system == "Linux":
-            subprocess.run(["aplay", filepath])  #command line audio file player
+            subprocess.run(["aplay", filepath])
         else:
             print("[ERROR] Unsupported OS for playback.")
-    except Exception as e:      #saves error message in variable e
-        print(f"[ERROR] Could not play audio: {e}")     #prints error message + actual error message 
+    except Exception as e:
+        print(f"[ERROR] Could not play audio: {e}")
 
 '''
 Function sorts and lists the wav files available.
@@ -353,7 +360,7 @@ Example usage:
 '''
 def list_wav_files(directory="output_audio"):
     try:
-        return sorted([f for f in os.listdir(directory) if f.endswith(".wav")]) #dispay all wav. files sorted alphabetically
+        return sorted([f for f in os.listdir(directory) if f.endswith(".wav")])
     except FileNotFoundError:
         print("[ERROR] output_audio/ folder not found.")
         return []
@@ -373,36 +380,34 @@ Available files are numbered for user to select. Function restarts after playing
 
 Example usage:
     playback_menu()
-'''
+''' 
 def playback_menu():
-    #continously show menu after playing audio file
-    while True:
-        print("\n=== Playback Menu ===")
-        files = list_wav_files()
-            
-        if not files:
-            print("[INFO] No .wav files found in 'output_audio/'.")
-            return
+    print("\n=== Playback Menu ===")
+    files = list_wav_files()
 
-        for i, f in enumerate(files):   #prints numbered list
-            print(f"{i + 1}. {f}")
-        
-        choice = input("Select a file to play (or 'q' to cancel): ").strip()
-        if choice.lower() == 'q':
-            print('[INFO] Exiting playback menu')
-            break #exit loop and quit function
+    if not files:
+        print("[INFO] No .wav files found in 'output_audio/'.")
+        return
 
-        if choice.isdigit():
-            index = int(choice) - 1
-            if 0 <= index < len(files): #error check if integer entered exceeds list size
-                filepath = os.path.join("output_audio", files[index])
-                print(f"[INFO] Playing: {filepath}\n")
-                play_audio_file(filepath)
-            else:
-                print("[ERROR] Invalid selection.")
+    for i, f in enumerate(files):
+        print(f"{i + 1}. {f}")
+    
+    choice = input("Select a file to play (or 'q' to cancel): ").strip()
+    if choice.lower() == 'q':
+        return
+
+    if choice.isdigit():
+        index = int(choice) - 1
+        if 0 <= index < len(files):
+            filepath = os.path.join("output_audio", files[index])
+            print(f"[INFO] Playing: {filepath}\n")
+            play_audio_file(filepath)
         else:
-            print("[ERROR] Invalid input.")
+            print("[ERROR] Invalid selection.")
+    else:
+        print("[ERROR] Invalid input.")
         
+
 '''
 Function prints menu for user in the terminal and returns the listed selection number.
 
@@ -426,20 +431,10 @@ def show_menu():
     print("3. Interrupt Recording\n")
     print("4. Exit\n")
     print("5. Playback Options\n")
-    selection = input("Select an option (1-5): \n")
-    try: 
-        selection_d = int(selection)
-        if 1 <= selection_d <= 5:
-            return selection_d
-        else:
-            print("[ERROR] Only input a number from 1-5")
-    except ValueError: 
-        print("[ERROR] Invalid input.\nOnly input a number from 1-5")
-
+    return input("Select an option (1-5): \n")
 
 def main():
     try:
-        #initialise serial port
         ser = serial.Serial(port=PORT, baudrate=BAUDRATE, bytesize=8, parity='N', stopbits=1, timeout=TIMEOUT)
         print(f"[INFO] Connected to {ser.name}\n")
 
@@ -456,9 +451,11 @@ def main():
                 break
             elif choice == '5':
                 playback_menu()
+            else:
+                print("[ERROR] Invalid selection. Please choose again.\n")
+
         ser.close()
 
-    #check for error when closing serial port
     except serial.SerialException as e:
         print(f"[ERROR] Serial error: {e}\n")
     except KeyboardInterrupt:
